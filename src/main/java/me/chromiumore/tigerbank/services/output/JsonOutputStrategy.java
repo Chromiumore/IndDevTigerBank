58ca892acb41554abdaa7eff96313cf5d8df8359
Chromiumore <parts.dima06@gmail.com>
2026-03-10 23:11:42 +0300

    json export id fix

--- src/main/java/me/chromiumore/tigerbank/services/output/JsonOutputStrategy.java
+++ src/main/java/me/chromiumore/tigerbank/services/output/JsonOutputStrategy.java
@@ -4,7 +4,11 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
+import me.chromiumore.tigerbank.entities.BaseEntity;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class JsonOutputStrategy extends OutputStrategy{
@@ -18,10 +22,12 @@ public class JsonOutputStrategy extends OutputStrategy{
             return "";
         }
 
+        List<BaseEntity> entities = new ArrayList<>(data.values());
+
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JSR310Module());
         try {
-            return objectMapper.writeValueAsString(data);
+            return objectMapper.writeValueAsString(entities);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
